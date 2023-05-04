c6691fcbb918451afa5817f8455a9f947b9e82e2
Abel Colina <abdv.vargas@gmail.com>
2023-05-03 23:49:15 -0400

    wip: made optimizations

---
+++ client/hooks/usePostHome.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import { postList } from '../services/api-post';
+import auth from '../auth/auth-helper';
+
+export default function usePostHome(user) {
+    const [postsHome, setPostsHome] = useState([]);
+    const [postHomeLoading, setPostHomeLoading] = useState(false);
+    const [transition, setTransition] = useState(false);
+
+    useEffect(() => {
+        const abortController = new AbortController();
+        const signal = abortController.signal;
+        setPostHomeLoading(true);
+    
+        postList({
+          params: {userId: user.id},
+          credentials: {divineMole: user.token},
+          signal
+        }).then(data => {
+          setPostHomeLoading(false);
+          if(data.error){
+            console.log(data.error);
+          }else{      
+            setTransition(true);
+            setPostsHome(data);
+          }
+        });
+    
+        return function cleanup() {
+          abortController.abort(); 
+        }
+
+      },[]);
+
+    return { postsHome, setPostsHome, postHomeLoading, setPostHomeLoading, transition }; 
+}
