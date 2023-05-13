793987eb8758ed897bf200ae8fff48b0aa149286
Abel Colina <abdv.vargas@gmail.com>
2023-05-12 20:13:47 -0400

    wip: trying deploy

--- webpack.config.client.js
+++ webpack.config.client.js
@@ -15,7 +15,7 @@ const config = {
     output: {
         path: path.join(CURRENT_WORKING_DIR, '/dist/'),
         filename: 'bundle.js',
-        publicPath: '/dist/'
+        publicPath: '/'
     },
     module: {
         rules: [
